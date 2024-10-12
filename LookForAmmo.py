07f5b6faaddca27d5ba4b78e5a18aa0df2a938ff
Aidan <aidan.brock.mine@outlook.com>
2024-10-12 17:27:59 +0100

    Made find ammo Function

---
+++ LookForAmmo.py
@@ -0,0 +1,16 @@
+from go import go
+from utilities import GetHeading, ServerMessageTypes
+
+def findAmmo(pickups :dict, Gameserver, player_x, player_y):
+    lowest = None
+    lowest_coords = None
+    for key, pickup in pickups:
+        if lowest == None: lowest =  pickup["TimeSeen"]
+        if lowest_coords == None: lowest = key
+
+        if pickup["Type"] == "Ammo":
+            if pickup["TimeSeen"] < lowest:
+                lowest = pickup["TimeSeen"]
+                lowest_coords = key
+    
+    return lowest_coords
