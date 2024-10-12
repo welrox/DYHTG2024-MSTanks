094493252e62e9b969cb95d74d6c194eb088c868
robertbrj <robert.brujban1@gmail.com>
2024-10-12 21:56:37 +0100

    hunt correctly

--- hunt.py
+++ hunt.py
@@ -3,6 +3,7 @@ import go
 import random
 
 def hunt(GameServer, player_x, player_y, heading):
-    #if CalculateDistance(player_x, player_y, 0, 0) > 3:
     go.go(GameServer, player_x, player_y, 0, 0)
-    GameServer.sendMessage(ServerMessageTypes.TURNTURRETTOHEADING, {'Amount': random.randint(0, 359)})
+
+    # GameServer.sendMessage(ServerMessageTypes.TURNTURRETTOHEADING, {'Amount': random.randint(0, 359)})
+    GameServer.sendMessage(ServerMessageTypes.TOGGLETURRETRIGHT)
