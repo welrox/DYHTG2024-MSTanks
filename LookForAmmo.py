592c8f72d08ea4db7e12b687572d1cd038ad3217
welrox <osamabadeeb2@gmail.com>
2024-10-12 18:18:02 +0100

    lookforammo fix v2

--- LookForAmmo.py
+++ LookForAmmo.py
@@ -4,10 +4,9 @@ def GetMostRecentlySeenAmmo(pickups :dict, Gameserver, player_x, player_y):
     lowest = None
     lowest_coords = None
     for key, pickup in pickups.items():
-        if lowest == None: lowest =  pickup["TimeSeen"]
-        if lowest_coords == None: lowest_coords = key
-
         if pickup["Type"] == "AmmoPickup":
+            if lowest == None: lowest =  pickup["TimeSeen"]
+            if lowest_coords == None: lowest_coords = key
             dist = CalculateDistance(player_x, player_y, key[0], key[1])
             if dist < lowest:
                 lowest = dist
