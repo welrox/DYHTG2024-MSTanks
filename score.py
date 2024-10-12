488ad83e7ca8baf5c93f163d130cd9b2f365376b
Zike_01 <zikekomakoma@gmail.com>
2024-10-12 19:10:50 +0100

    fixed scoring

--- score.py
+++ score.py
@@ -3,7 +3,6 @@ import go
 
 def score(GameServer, my_position):
     closest_goal = None
-    closest_dist = None
     blue = (0, 100)
     orange = (0, -100)
 
@@ -14,12 +13,12 @@ def score(GameServer, my_position):
 
     if blue_dist < orange_dist:
         closest_goal = blue
-        closest_dist = blue_dist
     else:
         closest_goal = orange
-        closest_dist = orange_dist
 
-    go.go(GameServer, my_position[0], my_position[1], closest_goal[0], closest_goal[1])
+    go.go(GameServer, my_position[0], my_position[1], closest_goal[0], closest_goal[1]*0.8)
+    go.go(GameServer, my_position[0], my_position[1], closest_goal[0], closest_goal[1]*1.5)
+    
 
     # heading = GetHeading(
     #     my_position[0], my_position[1], closest_goal[0], closest_goal[1]
